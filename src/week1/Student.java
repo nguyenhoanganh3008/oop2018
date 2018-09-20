71f950b2423154d3c4176b030363d562dc01904b
nguyenhoanganh3008 <nguyenhoanganh01a2@gmail.com>
2018-09-21 01:39:44 +0700

    jjjjjjj

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,16 +1,50 @@
 package week1;
 
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
+import java.sql.SQLOutput;
+
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+    private String name;
+    private int id;
+    private String group;
+    private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
-
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public void setGroup(String group){
+        this.group=group;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        name="Student";
+        id=000;
+        group="INT22041";
+        email="uet@vnu.edu.vn";
     }
 
     /**
@@ -19,20 +53,32 @@ public class Student {
      * @param sid
      * @param em
      */
-    Student(String n, String sid, String em) {
+    Student(String n, int sid, String em) {
         // TODO:
+        this.name= n;
+        this.id = sid;
+        this.email = em;
+        group="INT22041";
     }
 
     /**
      * Constructor 3
-     * @param s
+     * @param 's'
      */
     Student(Student s) {
         // TODO:
-    }
+    this.name=s.getName();
+    this.id=s.getId();
+    this.group=s.getGroup();
+    this.email=s.getEmail();
+
+  }
 
     String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        return "Name: " + this.name + "\n"
+                + "Id: " + this.id   + "\n"
+                + "Group: " + this.group + "\n"
+                + "Email: " + this.email;
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,16 +1,17 @@
 package week1;
 
 public class StudentManagement {
-
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    Student student[] = new Student[100];
 
-    public boolean sameGroup(Student s1, Student s2) {
+    public boolean sameGroup(Student s1, Student s2, Student s3) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        return s1.getGroup().equals(s2.getGroup());
     }
 
     void studentsByGroup() {
         // TODO:
+        
     }
 
     void removeStudent(String id) {
@@ -18,6 +19,32 @@ public class StudentManagement {
     }
 
     public static void main(String[] args) {
-        // TODO:
+
+        // TODO: them doi tuong
+
+        Student sv1 = new Student();
+        Student sv2 = new Student();
+        sv1.setName("Hoang Anh");
+        sv1.setId(17020577);
+        sv1.setGroup("INT2204");
+        sv1.setEmail("nguyenhoanganh01a2@gmail.com");
+        System.out.println("Name: " +sv1.getName());
+        System.out.println("Info: " + "\n" +sv1.getInfo());
+
+
+        Student s1 = new Student();
+        s1.setGroup("INT22041");
+        Student s2 = new Student();
+        s2.setGroup("INT22041");
+        Student s3 = new Student();
+        s3.setGroup("INT22042");
+
+        System.out.print("s1 cung group s2 : ");
+        System.out.println(s1.getGroup().equals(s2.getGroup()));
+        System.out.print("s3 cung group s1,s2 : ");
+        System.out.println(s1.getGroup().equals(s3.getGroup()));
+
+
+
     }
 }
