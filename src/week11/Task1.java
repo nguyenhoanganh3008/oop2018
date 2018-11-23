0b14cbba7d7c5e48531c25095ae437528ecdc399
nguyenhoanganh3008 <nguyenhoanganh01a2@gmail.com>
2018-11-23 11:07:12 +0700

    hihi

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,33 @@
 package week11;
 
 public class Task1 {
+    public static <T extends Comparable> void sort(T[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 1; j <arr.length ; j++) {
+                if(arr[j].compareTo( arr[j-1]) < 0){
+                    T a=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=a;
+                }
+            }
+        }
+    }
+    public static <T> void sout(T[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println("\n===========================");
+
+    }
+    public static void main(String[] args) {
+        Integer[] intarray = {3,12,2,34,1,6,7,0};
+        sort(intarray);
+        sout(intarray);
+        Character[] chars = {'h','o','a','n','g','a','n','h'};
+        sort(chars);
+        sout(chars);
+        Double[] doubles = {3.2,4.6,8.1,1.5,9.4,0.4,10.5};
+        sort(doubles);
+        sout(doubles);
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,27 @@
 package week11;
 
-public class Task2 {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task2<T> {
+    public <T extends Comparable> void max(ArrayList<T> arrayList){
+        T max = arrayList.get(0);
+        for (int i = 0; i <arrayList.size() ; i++) {
+            if(max.compareTo(arrayList.get(i))<0){
+                max=arrayList.get(i);
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> a = new ArrayList<>();
+        for (int i = 0; i <6 ; i++) {
+            int number = scanner.nextInt();
+            a.add(number);
+        }
+        Task2 task2 = new Task2();
+        task2.max(a);
+    }
 }
